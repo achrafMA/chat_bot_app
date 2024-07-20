738505843a9565346e9bcafc636a11e8af854b88
achrafMA <achrafmaataoui218@gmail.com>
2024-07-20 23:51:23 +0100

    s

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:chat_app/chat.page.dart';
+import 'package:flutter/material.dart';
+
+import 'chat.bot.page.dart';
+import 'home.page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {"/chat": (context) => ChatBotPage()},
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        indicatorColor: Colors.white,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 400,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: Card.outlined(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Image(image: AssetImage("images/img.png")),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      controller: loginController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 1,
+                            color: Colors.teal,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: Icon(Icons.visibility),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            width: 1,
+                            color: Colors.teal,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          String usename = loginController.text;
+                          String password = passwordController.text;
+                          if (usename == "admin" && password == "1234") {
+                            Navigator.of(context).pop();
+                            Navigator.pushNamed(context, "/chat");
+                          }
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Theme.of(context).indicatorColor),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
