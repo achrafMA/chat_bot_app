430fb30db283ff86ac86dcb5bdcc162af38eb193
achrafMA <achrafmaataoui218@gmail.com>
2024-07-20 23:30:25 +0100

    Revert "1st"
    This reverts commit 025b0dd9

--- README.md
+++
@@ -1,4 +0,0 @@
-# chat_bot_app
-username : user
-password : password
-![img.png](img.png)
--- android/app/src/main/kotlin/com/example/chat_bot_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chat_bot_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
